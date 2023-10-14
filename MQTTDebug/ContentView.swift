4c80577e22a48227367189d222ee038334fff1a8
ALsJourney <alsjourneydev@hotmail.com>
2023-10-14 12:32:45 +0200

    Initial Commit

---
+++ MQTTDebug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MQTTDebug
+//
+//  Created by Aldin Cimpo on 14.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MQTTDebug/MQTTDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTTDebugApp.swift
+//  MQTTDebug
+//
+//  Created by Aldin Cimpo on 14.10.23.
+//
+
+import SwiftUI
+
+@main
+struct MQTTDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
