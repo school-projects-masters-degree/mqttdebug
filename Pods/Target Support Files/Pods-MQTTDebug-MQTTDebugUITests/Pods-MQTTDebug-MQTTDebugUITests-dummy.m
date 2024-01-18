b3eb96a4ad789b93dfc8df3c42027ae071511171
ALsJourney <alsjourneydev@hotmail.com>
2024-01-18 19:54:07 +0100

    Remove Platforms, except iphone

--- Pods/Target Support Files/Pods-MQTTDebug-MQTTDebugUITests/Pods-MQTTDebug-MQTTDebugUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MQTTDebug_MQTTDebugUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MQTTDebug_MQTTDebugUITests
-@end
--- Pods/Target Support Files/Pods-MQTTDebugTests/Pods-MQTTDebugTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MQTTDebugTests : NSObject
-@end
-@implementation PodsDummy_Pods_MQTTDebugTests
-@end
